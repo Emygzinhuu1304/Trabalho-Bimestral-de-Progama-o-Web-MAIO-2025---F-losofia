9108c472268eff3fe3fa3bfeb9f94a1808052e77
Miguel Magalhães <miguelmagoliveira@gmail.com>
2025-06-16 12:04:02 -0300

    Delete PW2 MAIO/User directory

--- PW2 MAIO/User/js2/login.js
+++
@@ -1,7 +0,0 @@
-function handleMenuClick(opcao) {
-  if (opcao === 'usuario') {
-    window.location.href = '/login/index.html'; // Caminho correto para a tela de login
-  } else if (opcao === 'professor') {
-    console.log('Sobre o Professor clicado');
-  }
-}
